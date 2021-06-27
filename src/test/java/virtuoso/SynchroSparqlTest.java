2213bd1e0c27a95ccec8f6136e3e9174e9cc9916
Firas <firas.gabsi@gmail.com>
2021-06-27 20:28:29 +0200

    Optimisation de l'intégration de Virtuoso

--- src/test/java/virtuoso/SynchroSparqlTest.java
+++ src/test/java/virtuoso/SynchroSparqlTest.java
@@ -1,12 +1,12 @@
 package virtuoso;
 
 import fr.cnrs.opentheso.bdd.helper.nodes.NodeLangTheso;
-import fr.cnrs.opentheso.virtuoso.SparqlStruct;
 import org.junit.Test;
 import fr.cnrs.opentheso.virtuoso.SynchroSparql;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class SynchroSparqlTest {
 
@@ -14,15 +14,6 @@ public class SynchroSparqlTest {
     public void runVirtuosoTest() {
 
         SynchroSparql synchroSparql = new SynchroSparql();
-
-        SparqlStruct sparqlStruct = new SparqlStruct();
-        sparqlStruct.setAdresseServeur("http://localhost");
-        sparqlStruct.setNom_d_utilisateur("dba");
-        sparqlStruct.setMot_de_passe("dba");
-        sparqlStruct.setThesaurus("th1");
-        sparqlStruct.setGraph("Firas_TEST");
-
-        synchroSparql.setSparqlStruct(sparqlStruct);
 
         NodeLangTheso nodeLangTheso = new NodeLangTheso();
         nodeLangTheso.setCode("fr");
