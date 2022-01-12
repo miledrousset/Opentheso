49deb5660ed1ddcacbfc7a473bc1e32d74dae315
Firas <firas.gabsi@gmail.com>
2022-01-12 14:45:36 +0100

    Ajout du module alignement

---
+++ src/main/java/fr/cnrs/opentheso/bean/alignment/AlignementElement.java
@@ -0,0 +1,53 @@
+package fr.cnrs.opentheso.bean.alignment;
+
+public class AlignementElement {
+
+    private String idConceptOrig;
+    private String labelConceptOrig;
+    private String tradConceptOrig;
+    private String typeAlignement;
+    private String labelConceptCible;
+
+    public String getIdConceptOrig() {
+        return idConceptOrig;
+    }
+
+    public void setIdConceptOrig(String idConceptOrig) {
+        this.idConceptOrig = idConceptOrig;
+    }
+
+    public String getLabelConceptOrig() {
+        return labelConceptOrig;
+    }
+
+    public void setLabelConceptOrig(String labelConceptOrig) {
+        this.labelConceptOrig = labelConceptOrig;
+    }
+
+    public String getTradConceptOrig() {
+        return tradConceptOrig;
+    }
+
+    public void setTradConceptOrig(String tradConceptOrig) {
+        this.tradConceptOrig = tradConceptOrig;
+    }
+
+    public String getTypeAlignement() {
+        return typeAlignement;
+    }
+
+    public void setTypeAlignement(String typeAlignement) {
+        this.typeAlignement = typeAlignement;
+    }
+
+    public String getLabelConceptCible() {
+        return labelConceptCible;
+    }
+
+    public void setLabelConceptCible(String labelConceptCible) {
+        this.labelConceptCible = labelConceptCible;
+    }
+    
+    
+    
+}
