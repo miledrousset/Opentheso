db9a5ad4fdb5af4f776a307de4b825b8255ac88e
Julien LINGET <julien.linget@protonmail.com>
2023-05-16 16:17:03 +0200

    fix/openapi: Correction de directFetchConcept

--- src/main/java/fr/cnrs/opentheso/ws/openapi/helper/ConceptHelper.java
+++ src/main/java/fr/cnrs/opentheso/ws/openapi/helper/ConceptHelper.java
@@ -18,7 +18,7 @@ public class ConceptHelper {
 
         try(HikariDataSource ds = connect()) {
             if (ds == null) return ResponseHelper.errorResponse(Response.Status.SERVICE_UNAVAILABLE, "No connection to server", format);
-            datas = restRDFHelper.exportGroup(ds, id, HeaderHelper.removeCharset(format));
+            datas = restRDFHelper.exportConcept(ds, id, HeaderHelper.removeCharset(format));
             if (datas == null) return ResponseHelper.errorResponse(Response.Status.NOT_FOUND, "Concept not found", format);
             return ResponseHelper.response(Response.Status.OK, datas, format);
         }
