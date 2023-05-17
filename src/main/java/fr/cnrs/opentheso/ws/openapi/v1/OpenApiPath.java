203cabb32cb5dd0630b882c60465b5a1bd7b4dfc
Julien LINGET <julien.linget@protonmail.com>
2023-05-17 16:40:49 +0200

    fix/openapi: Déplacement de la fonction getUriFromArk

--- src/main/java/fr/cnrs/opentheso/ws/openapi/v1/OpenApiPath.java
+++ src/main/java/fr/cnrs/opentheso/ws/openapi/v1/OpenApiPath.java
@@ -5,17 +5,26 @@
  */
 package fr.cnrs.opentheso.ws.openapi.v1;
 
+import com.zaxxer.hikari.HikariDataSource;
+import fr.cnrs.opentheso.ws.RestRDFHelper;
+import fr.cnrs.opentheso.ws.Rest_new;
 import fr.cnrs.opentheso.ws.openapi.helper.ApiKeyHelper;
 import fr.cnrs.opentheso.ws.openapi.helper.ApiKeyState;
 import fr.cnrs.opentheso.ws.openapi.helper.CustomMediaType;
+import static fr.cnrs.opentheso.ws.openapi.helper.CustomMediaType.APPLICATION_JSON_UTF_8;
+import static fr.cnrs.opentheso.ws.openapi.helper.DataHelper.connect;
+import fr.cnrs.opentheso.ws.openapi.helper.MessageHelper;
 import fr.cnrs.opentheso.ws.openapi.helper.ResponseHelper;
 import fr.cnrs.opentheso.ws.openapi.scanner.io.swagger.v3.jaxrs2.integration.resources.BaseOpenApiResource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.ServletConfig;
 import javax.ws.rs.GET;
@@ -121,6 +130,45 @@ public class OpenApiPath extends BaseOpenApiResource {
         Response myResponse = ResponseHelper.response(Response.Status.OK, builder.build().toString(), CustomMediaType.APPLICATION_JSON_UTF_8);
         
         return myResponse;
+    }
+    
+    
+    @Path("/redirect/ark:/{naan}/{idArk}")
+    @GET
+    @Produces({APPLICATION_JSON_UTF_8})
+    @Operation(summary = "getUriFromArk.summary",
+            description = "getUriFromArk.description",
+            tags = {"Ark"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "getUriFromArk.200.description"),
+                @ApiResponse(responseCode = "307", description = "getUriFromArk.307.description"),
+                @ApiResponse(responseCode = "400", description = "responses.400.description"),
+                @ApiResponse(responseCode = "404", description = "getUriFromArk.404.description", content = {
+            @Content(mediaType = APPLICATION_JSON_UTF_8)
+        }),
+                @ApiResponse(responseCode = "500", description = "responses.500.description")
+            })
+    public Response getUriFromArk(
+            @Parameter(name = "naan", in = ParameterIn.PATH, schema = @Schema(type = "string"), description = "getUriFromArk.naan.description") @PathParam("naan") String naan,
+            @Parameter(name = "idArk", in = ParameterIn.PATH, schema = @Schema(type = "string"), description = "getUriFromArk.idArk.description") @PathParam("idArk") String arkId
+    ) {
+        String webUrl;
+        try (HikariDataSource ds = connect()) {
+            if (ds == null) {
+                return ResponseHelper.errorResponse(Response.Status.SERVICE_UNAVAILABLE, "No database connection", APPLICATION_JSON_UTF_8);
+            }
+
+            RestRDFHelper restRDFHelper = new RestRDFHelper();
+            webUrl = restRDFHelper.getUrlFromIdArk(ds, naan, arkId);
+            if (webUrl == null) {
+                return ResponseHelper.errorResponse(Response.Status.NOT_FOUND, "Ark ID not found", APPLICATION_JSON_UTF_8);
+            }
+            URI uri = new URI(webUrl);
+            return Response.temporaryRedirect(uri).build();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(OpenApiPath.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ResponseHelper.errorResponse(Response.Status.INTERNAL_SERVER_ERROR, "Internal server error", APPLICATION_JSON_UTF_8);
     }
 
 }
--- src/main/java/fr/cnrs/opentheso/ws/openapi/v1/routes/concept/ConceptPath.java
+++ src/main/java/fr/cnrs/opentheso/ws/openapi/v1/routes/concept/ConceptPath.java
@@ -20,15 +20,10 @@ import javax.ws.rs.core.Response;
 import static fr.cnrs.opentheso.ws.openapi.helper.ConceptHelper.directFetchConcept;
 import static fr.cnrs.opentheso.ws.openapi.helper.CustomMediaType.*;
 import static fr.cnrs.opentheso.ws.openapi.helper.DataHelper.connect;
-import fr.cnrs.opentheso.ws.openapi.helper.MessageHelper;
 import static fr.cnrs.opentheso.ws.openapi.helper.MessageHelper.emptyMessage;
 import fr.cnrs.opentheso.ws.openapi.helper.ResponseHelper;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.QueryParam;
 
 /**
@@ -104,43 +99,6 @@ public class ConceptPath {
         }
 
         return ResponseHelper.response(Response.Status.OK, datas, APPLICATION_JSON_UTF_8);
-    }
-
-    @Path("/redirect/ark:/{naan}/{idArk}")
-    @GET
-    @Produces({APPLICATION_JSON_UTF_8})
-    @Operation(summary = "getUriFromArk.summary",
-            description = "getUriFromArk.description",
-            tags = {"Concept", "Ark"},
-            responses = {
-                @ApiResponse(responseCode = "307", description = "getUriFromArk.200.description"),
-                @ApiResponse(responseCode = "400", description = "responses.400.description"),
-                @ApiResponse(responseCode = "404", description = "getUriFromArk.404.description", content = {
-            @Content(mediaType = APPLICATION_JSON_UTF_8)
-        }),
-                @ApiResponse(responseCode = "500", description = "responses.500.description")
-            })
-    public Response getUriFromArk(
-            @Parameter(name = "naan", in = ParameterIn.PATH, schema = @Schema(type = "string"), description = "getUriFromArk.naan.description") @PathParam("naan") String naan,
-            @Parameter(name = "idArk", in = ParameterIn.PATH, schema = @Schema(type = "string"), description = "getUriFromArk.idArk.description") @PathParam("idArk") String idArk
-    ) {
-        String webUrl;
-        try (HikariDataSource ds = connect()) {
-            if (ds == null) {
-                return ResponseHelper.errorResponse(Response.Status.SERVICE_UNAVAILABLE, MessageHelper.errorMessage("No database connection", APPLICATION_JSON_UTF_8), APPLICATION_JSON_UTF_8);
-            }
-
-            RestRDFHelper restRDFHelper = new RestRDFHelper();
-            webUrl = restRDFHelper.getUrlFromIdArk(ds, naan, idArk);
-            if (webUrl == null) {
-                return ResponseHelper.errorResponse(Response.Status.NOT_FOUND, MessageHelper.errorMessage("Ark ID not found", APPLICATION_JSON_UTF_8), APPLICATION_JSON_UTF_8);
-            }
-            URI uri = new URI(webUrl);
-            return Response.temporaryRedirect(uri).build();
-        } catch (URISyntaxException ex) {
-            Logger.getLogger(ConceptPath.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return ResponseHelper.errorResponse(Response.Status.INTERNAL_SERVER_ERROR, MessageHelper.errorMessage("Internal server error", APPLICATION_JSON_UTF_8), APPLICATION_JSON_UTF_8);
     }
 
 }
