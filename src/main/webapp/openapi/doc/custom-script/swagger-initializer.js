8a9dabee0b3636d3bb2ac6392e36e008f98a771c
Julien LINGET <julien.linget+github@proton.me>
2023-06-06 13:41:33 +0200

    fix/openapi: Suppression du sélecteur de version

--- src/main/webapp/openapi/doc/custom-script/swagger-initializer.js
+++ src/main/webapp/openapi/doc/custom-script/swagger-initializer.js
@@ -153,7 +153,7 @@ const loadQueryVersion = () => {
         const parameters = new URLSearchParams(queryString);
         if (parameters.has("version") && (AVAILABLE_VERSION.indexOf(parameters.get("version").toLowerCase()) !== -1)) { 
             VERSION = parameters.get("version").toLowerCase();
-            changeVersionSelectorDefault(VERSION);
+            // changeVersionSelectorDefault(VERSION);
         }
     } 
 };
@@ -192,7 +192,7 @@ const displayServerDoc = (title, version, lang) => {
     changeTitle(title);
     window.ui = uiBundle(generateURL(version, lang));
     hideSearchBar();
-    addVersionSelectorListener();
+    // addVersionSelectorListener();
     addLangSelectorListener();
 };
 
@@ -202,13 +202,13 @@ let VERSION = "v1";
 let LANG = "fr";
 const BASE_URL = window.location.href.split("/doc")[0]  + "/";
 const AVAILABLE_LANG = [];
-const AVAILABLE_VERSION = getAvailableVersions();
+const AVAILABLE_VERSION = ["v1"];
 
 window.onload = function() {
     fetchAvailableLanguages()
             .then(() => {
                 loadQueryLang();
-                loadQueryVersion();
+                // loadQueryVersion();
                 displayServerDoc("Opentheso2 - API" ,VERSION, LANG);
             });
 };
