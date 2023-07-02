be44fd20a1ee07e067d5ddcdd4b4ee4673c89227
GABSI <firas.gabsi@gmail.com>
2023-07-02 10:10:13 +0100

    Petites rectifcations du module alignement automatique

--- src/main/java/fr/cnrs/opentheso/bean/alignment/AlignmentBean.java
+++ src/main/java/fr/cnrs/opentheso/bean/alignment/AlignmentBean.java
@@ -43,7 +43,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.annotation.PreDestroy;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
