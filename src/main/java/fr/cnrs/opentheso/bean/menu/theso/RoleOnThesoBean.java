0207347c044401542079a199eaeaeaf9a3780fa4
GABSI <firas.gabsi@gmail.com>
2023-07-04 00:06:38 +0100

    Petites rectifcations du module alignement automatique

--- src/main/java/fr/cnrs/opentheso/bean/menu/theso/RoleOnThesoBean.java
+++ src/main/java/fr/cnrs/opentheso/bean/menu/theso/RoleOnThesoBean.java
@@ -420,7 +420,12 @@ public class RoleOnThesoBean implements Serializable {
         } else {
             int idGroup = new UserHelper().getGroupOfThisTheso(connect.getPoolConnexion(), selectedTheso.getCurrentIdTheso());
             nodeUserRoleGroup = getUserRoleOnThisGroup(idGroup);
+        }
+
+        if (ObjectUtils.isNotEmpty(nodeUserRoleGroup)) {
             setRole();
+        } else {
+            isAdminOnThisTheso = false;
         }
     }
 
