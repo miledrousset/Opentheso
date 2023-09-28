c15dfa3d67110a102638a3d105f50614b8b6d7a8
miledrousset <miledrousset@mbp-de-miled-2-001>
2023-09-28 06:52:35 +0200

    petits ajustements suite au merge

--- src/main/java/fr/cnrs/opentheso/bean/importexport/ExportFileBean.java
+++ src/main/java/fr/cnrs/opentheso/bean/importexport/ExportFileBean.java
@@ -7,6 +7,7 @@ import fr.cnrs.opentheso.bdd.helper.PreferencesHelper;
 import fr.cnrs.opentheso.bdd.helper.nodes.NodePreference;
 import fr.cnrs.opentheso.bdd.helper.nodes.NodeTree;
 import fr.cnrs.opentheso.bdd.helper.nodes.group.NodeGroupLabel;
+import fr.cnrs.opentheso.bdd.tools.StringPlus;
 import fr.cnrs.opentheso.bean.candidat.CandidatBean;
 import fr.cnrs.opentheso.bean.candidat.dto.CandidatDto;
 import fr.cnrs.opentheso.bean.leftbody.viewtree.Tree;
@@ -438,6 +439,17 @@ public class ExportFileBean implements Serializable {
         if (nodePreference == null) {
             return null;
         }
+        if(StringUtils.isEmpty(nodePreference.getOriginalUri())){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "",
+                    "Veuillez paramétrer l'URI de l'export dans les préférences !"));
+            return null;
+        }
+        if("null".equalsIgnoreCase(nodePreference.getOriginalUri())){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "",
+                    "Veuillez paramétrer l'URI de l'export dans les préférences !"));            
+            return null;            
+        }
+        
         UriHelper uriHelper = new UriHelper(connect.getPoolConnexion(), nodePreference, viewExportBean.getNodeIdValueOfTheso().getId()); 
 
         
