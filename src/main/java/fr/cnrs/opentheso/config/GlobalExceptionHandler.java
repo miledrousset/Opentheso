b7b49f7d25c3632c0ecc4a56962c68d79ddf7836
GABSI <firas.gabsi@gmail.com>
2024-09-26 00:49:45 +0200

    Ajout d'une redirection dans le cas où il y a une erreur 500 et 404

--- src/main/java/fr/cnrs/opentheso/config/GlobalExceptionHandler.java
+++ src/main/java/fr/cnrs/opentheso/config/GlobalExceptionHandler.java
@@ -1,17 +1,41 @@
 package fr.cnrs.opentheso.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class) // Gère toutes les exceptions
-    public ModelAndView handleException(Exception ex, Model model) {
+    // Gestion des erreurs 500
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest request, Exception ex, Model model) {
+        if (getHttpStatus(request).equals(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return new ModelAndView("redirect:/errorPages/error500.xhtml");
+        }
+
         model.addAttribute("errorMessage", ex.getMessage());
-        return new ModelAndView("redirect:/errorPages/error500.xhtml");
+        return new ModelAndView("redirect:/errorPages/errorGeneric.xhtml");
+    }
+
+    // Gestion des erreurs 404
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handleNotFoundException(NoHandlerFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return new ModelAndView("redirect:/errorPages/error404.xhtml");
+    }
+
+    private HttpStatus getHttpStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
     }
 }
