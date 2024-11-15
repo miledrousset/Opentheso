b51e5a618dd0f2a9c084c5855e44a71a5a47a4ca
Rousset Miled <miled.rousset@mom.fr>
2024-11-15 14:03:05 +0100

    Correction de l'export avec optimisation (la fonction des collections qui posaient problème avec LOWER).

--- src/main/resources/changelog/v24.11/24-11_07.sql
+++ src/main/resources/changelog/v24.11/24-11_07.sql
@@ -1,328 +1,40 @@
+DROP FUNCTION IF EXISTS public.opentheso_get_groups(character varying, character varying);
 
-
-
-
--- FUNCTION: public.opentheso_get_concepts(character varying, character varying)
-
- DROP FUNCTION IF EXISTS public.opentheso_get_concepts(character varying, character varying);
-
-CREATE OR REPLACE FUNCTION public.opentheso_get_concepts(
+CREATE OR REPLACE FUNCTION public.opentheso_get_groups(
 	id_theso character varying,
-	path character varying)
-    RETURNS SETOF record
+	id_con character varying)
+    RETURNS TABLE(group_id text, group_id_ark text, group_id_handle character varying, group_id_doi character varying)
     LANGUAGE 'plpgsql'
     COST 100
     VOLATILE PARALLEL UNSAFE
     ROWS 1000
 
 AS $BODY$
-DECLARE
+begin
+return query
+SELECT concept_group.idgroup, concept_group.id_ark, concept_group.id_handle, concept_group.id_doi
+FROM concept_group_concept, concept_group
+WHERE
+    concept_group.idgroup = concept_group_concept.idgroup
+  AND concept_group.idthesaurus = concept_group_concept.idthesaurus
+  AND concept_group_concept.idthesaurus = id_theso
+  AND concept_group_concept.idconcept = id_con;
 
-seperateur constant varchar := '##';
-	sous_seperateur constant varchar := '@@';
-
-	rec record;
-	con record;
-	theso_rec record;
-	traduction_rec record;
-	altLab_rec record;
-	altLab_hiden_rec record;
-	geo_rec record;
-	group_rec record;
-	note_concept_rec record;
-	note_term_rec record;
-	relation_rec record;
-	alignement_rec record;
-	img_rec record;
-	vote_record record;
-	message_record record;
-	cadidat_record record;
-	replace_rec record;
-	replacedBy_rec record;
-	facet_rec record;
-    externalResource_rec record;
-	contributor_rec record;
-
-	tmp text;
-	uri text;
-	local_URI text;
-	prefLab VARCHAR;
-	altLab VARCHAR;
-	altLab_hiden VARCHAR;
-	membre text;
-	definition text;
-	secopeNote text;
-	note text;
-	historyNote text;
-	example text;
-	changeNote text;
-	editorialNote text;
-	narrower text;
-	broader text;
-	related text;
-	exactMatch text;
-	closeMatch text;
-	broadMatch text;
-	relatedMatch text;
-	narrowMatch text;
-	img text;
-	creator text;
-	contributor text;
-	replaces text;
-	replacedBy text;
-	facets text;
-    externalResources text;
-    gpsData text;
-BEGIN
-
-SELECT * INTO theso_rec FROM preferences where id_thesaurus = id_theso;
-
-FOR con IN SELECT * FROM concept WHERE id_thesaurus = id_theso AND status != 'CA'
-    LOOP
-        -- URI
-	uri = opentheso_get_uri(theso_rec.original_uri_is_ark, con.id_ark, theso_rec.original_uri, theso_rec.original_uri_is_handle,
-					 con.id_handle, theso_rec.original_uri_is_doi, con.id_doi, con.id_concept, id_theso, path);
-
--- LocalUri
-local_URI = path || '/?idc=' || con.id_concept || '&idt=' || id_theso;
-
-        -- PrefLab
-        prefLab = '';
-FOR traduction_rec IN SELECT * FROM opentheso_get_all_preflabel(id_theso, con.id_concept)
-                                        LOOP
-    prefLab = prefLab || traduction_rec.term_lexical_value || sous_seperateur || traduction_rec.term_lang || seperateur;
-END LOOP;
-
-		-- altLab
-		altLab = '';
-FOR altLab_rec IN SELECT * FROM opentheso_get_alter_term(id_theso, con.id_concept, false)
-                                    LOOP
-    altLab = altLab || altLab_rec.alter_term_lexical_value || sous_seperateur || altLab_rec.alter_term_lang || seperateur;
-END LOOP;
-
-		-- altLab hiden
-		altLab_hiden = '';
-FOR altLab_hiden_rec IN SELECT * FROM opentheso_get_alter_term(id_theso, con.id_concept, true)
-                                          LOOP
-    altLab_hiden = altLab_hiden || altLab_hiden_rec.alter_term_lexical_value || sous_seperateur || altLab_hiden_rec.alter_term_lang || seperateur;
-END LOOP;
-
-		-- Notes
-		note = '';
-		example = '';
-		changeNote = '';
-		secopeNote = '';
-		definition = '';
-		historyNote = '';
-		editorialNote = '';
-FOR note_concept_rec IN SELECT * FROM opentheso_get_notes(id_theso, con.id_concept)
-                                          LOOP
-    IF (note_concept_rec.note_notetypecode = 'note') THEN
-				note = note || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-ELSIF (note_concept_rec.note_notetypecode = 'scopeNote') THEN
-				secopeNote = secopeNote || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-			ELSIF (note_concept_rec.note_notetypecode = 'historyNote') THEN
-				historyNote = historyNote || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-			ELSIF (note_concept_rec.note_notetypecode = 'definition') THEN
-				definition = definition || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-			ELSIF (note_concept_rec.note_notetypecode = 'example') THEN
-				example = example || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-			ELSIF (note_concept_rec.note_notetypecode = 'changeNote') THEN
-				changeNote = changeNote || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-			ELSIF (note_concept_rec.note_notetypecode = 'editorialNote') THEN
-				editorialNote = editorialNote || note_concept_rec.note_lexicalvalue || sous_seperateur || note_concept_rec.note_lang || seperateur;
-END IF;
-END LOOP;
-
-		-- Narrower
-		narrower = '';
-		broader = '';
-		related = '';
-FOR relation_rec IN SELECT * FROM opentheso_get_relations(id_theso, con.id_concept)
-                                      LOOP
-    tmp = opentheso_get_uri(theso_rec.original_uri_is_ark, relation_rec.relationship_id_ark, theso_rec.original_uri,
-					theso_rec.original_uri_is_handle, relation_rec.relationship_id_handle, theso_rec.original_uri_is_doi,
-					relation_rec.relationship_id_doi, relation_rec.relationship_id_concept, id_theso, path)
-					|| sous_seperateur || relation_rec.relationship_role || sous_seperateur || relation_rec.relationship_id_concept || sous_seperateur ;
-
-IF (relation_rec.relationship_role = 'NT' OR relation_rec.relationship_role = 'NTP' OR relation_rec.relationship_role = 'NTI'
-					OR relation_rec.relationship_role = 'NTG') THEN
-				narrower = narrower || tmp || seperateur;
-			ELSIF (relation_rec.relationship_role = 'BT' OR relation_rec.relationship_role = 'BTP' OR relation_rec.relationship_role = 'BTI'
-					OR relation_rec.relationship_role = 'BTG') THEN
-				broader = broader || tmp || seperateur;
-			ELSIF (relation_rec.relationship_role = 'RT' OR relation_rec.relationship_role = 'RHP' OR relation_rec.relationship_role = 'RPO') THEN
-				related = related || tmp || seperateur;
-END IF;
-END LOOP;
-
-		-- Alignement
-		exactMatch = '';
-		closeMatch = '';
-		broadMatch = '';
-		relatedMatch = '';
-		narrowMatch = '';
-FOR alignement_rec IN SELECT * FROM opentheso_get_alignements(id_theso, con.id_concept)
-                                        LOOP
-    if (alignement_rec.alig_id_type = 1) THEN
-				exactMatch = exactMatch || alignement_rec.alig_uri_target || seperateur;
-ELSEIF (alignement_rec.alig_id_type = 2) THEN
-				closeMatch = closeMatch || alignement_rec.alig_uri_target || seperateur;
-		    ELSEIF (alignement_rec.alig_id_type = 3) THEN
-				broadMatch = broadMatch || alignement_rec.alig_uri_target || seperateur;
-			ELSEIF (alignement_rec.alig_id_type = 4) THEN
-				relatedMatch = relatedMatch || alignement_rec.alig_uri_target || seperateur;
-			ELSEIF (alignement_rec.alig_id_type = 5) THEN
-				narrowMatch = narrowMatch || alignement_rec.alig_uri_target || seperateur;
-END IF;
-END LOOP;
-
-		-- geo:alt && geo:long
-        gpsData = '';
-FOR geo_rec IN SELECT * FROM opentheso_get_gps(id_theso, con.id_concept)
-                                 LOOP
-    gpsData = gpsData || geo_rec.gps_latitude || sous_seperateur || geo_rec.gps_longitude || seperateur;
-END LOOP;
-
-
-
-
-
-
-
-
-
-
-membre = '';
-FOR group_rec IN SELECT * FROM opentheso_get_groups(id_theso, con.id_concept)
-                                   LOOP
-                               -- Log du début de l'itération avec les valeurs de group_id, group_id_ark, et group_id_handle
-    RAISE NOTICE 'Traitement de group_id : %, group_id_ark : %, group_id_handle : %', group_rec.group_id, group_rec.group_id_ark, group_rec.group_id_handle;
-
-IF (theso_rec.original_uri_is_ark = true AND group_rec.group_id_ark IS NOT NULL AND group_rec.group_id_ark != '') THEN
-        membre = membre || theso_rec.original_uri || '/' || group_rec.group_id_ark || seperateur;
-        RAISE NOTICE 'Condition 1 (ARK avec URI original), membre : %', membre;
-
-    ELSIF (theso_rec.original_uri_is_ark = true AND (group_rec.group_id_ark IS NULL OR group_rec.group_id_ark = '')) THEN
-        membre = membre || path || '/?idg=' || group_rec.group_id || '&idt=' || id_theso || seperateur;
-        RAISE NOTICE 'Condition 2 (ARK sans URI), membre : %', membre;
-
-    ELSIF (group_rec.group_id_handle IS NOT NULL AND group_rec.group_id_handle != '') THEN
-        membre = membre || 'https://hdl.handle.net/' || group_rec.group_id_handle || seperateur;
-        RAISE NOTICE 'Condition 3 (Handle), membre : %', membre;
-
-    ELSIF (theso_rec.original_uri IS NOT NULL AND theso_rec.original_uri != '') THEN
-        membre = membre || theso_rec.original_uri || '/?idg=' || group_rec.group_id || '&idt=' || id_theso || seperateur;
-        RAISE NOTICE 'Condition 4 (URI original sans ARK), membre : %', membre;
-
-ELSE
-        membre = membre || path || '/?idc=' || group_rec.group_id || '&idt=' || id_theso || seperateur;
-        RAISE NOTICE 'Condition 5 (Autre cas), membre : %', membre;
-END IF;
-
-    -- Log pour la fin de l'itération
-    RAISE NOTICE 'Fin de traitement pour group_id : %, membre actuel : %', group_rec.group_id, membre;
-END LOOP;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		-- Images
-		img = '';
-FOR img_rec IN SELECT * FROM opentheso_get_images(id_theso, con.id_concept)
-                                 LOOP
-    img = img || img_rec.name || sous_seperateur || img_rec.copyright || sous_seperateur || img_rec.url || sous_seperateur || COALESCE(img_rec.creator, '') || seperateur;
-END LOOP;
-
-        -- Agent DcTerms
-        creator = '';
-        contributor = '';
-SELECT value INTO creator FROM concept_dcterms WHERE id_concept = con.id_concept and id_thesaurus = con.id_thesaurus and name = 'creator';
-
-FOR contributor_rec IN SELECT value
-                       FROM concept_dcterms
-                       WHERE id_concept = con.id_concept
-                         AND id_thesaurus = con.id_thesaurus
-                         AND name = 'contributor'
-                           LOOP
-                    IF (contributor != '') THEN
-                        contributor = contributor || seperateur;
-END IF;
-                    contributor = contributor || contributor_rec.value;
-END LOOP;
-
-        replaces = '';
-FOR replace_rec IN SELECT id_concept1, id_ark, id_handle, id_doi
-                   FROM concept_replacedby, concept
-                   WHERE concept.id_concept = concept_replacedby.id_concept1
-                     AND concept.id_thesaurus = concept_replacedby.id_thesaurus
-                     AND concept_replacedby.id_concept2 = con.id_concept
-                     AND concept_replacedby.id_thesaurus = id_theso
-                       LOOP
-			replaces = replaces || opentheso_get_uri(theso_rec.original_uri_is_ark, replace_rec.id_ark, theso_rec.original_uri,
-					theso_rec.original_uri_is_handle, replace_rec.id_handle, theso_rec.original_uri_is_doi,
-					replace_rec.id_doi, replace_rec.id_concept1, id_theso, path) || seperateur;
-END LOOP;
-
-		replacedBy = '';
-FOR replacedBy_rec IN SELECT id_concept2, id_ark, id_handle, id_doi
-                      FROM concept_replacedby, concept
-                      WHERE concept.id_concept = concept_replacedby.id_concept2
-                        AND concept.id_thesaurus = concept_replacedby.id_thesaurus
-                        AND concept_replacedby.id_concept1 = con.id_concept
-                        AND concept_replacedby.id_thesaurus = id_theso
-                          LOOP
-			replacedBy = replacedBy || opentheso_get_uri(theso_rec.original_uri_is_ark, replacedBy_rec.id_ark, theso_rec.original_uri,
-					theso_rec.original_uri_is_handle, replacedBy_rec.id_handle, theso_rec.original_uri_is_doi,
-					replacedBy_rec.id_doi, replacedBy_rec.id_concept2, id_theso, path) || seperateur;
-END LOOP;
-
-		facets = '';
-FOR facet_rec IN SELECT thesaurus_array.id_facet
-                 FROM thesaurus_array
-                 WHERE thesaurus_array.id_thesaurus = id_theso
-                   AND thesaurus_array.id_concept_parent = con.id_concept
-                     LOOP
-			facets = facets || facet_rec.id_facet || seperateur;
-END LOOP;
-
-		externalResources = '';
-FOR externalResource_rec IN SELECT external_resources.external_uri
-                            FROM external_resources
-                            WHERE external_resources.id_thesaurus = id_theso
-                              AND external_resources.id_concept = con.id_concept
-                                LOOP
-            externalResources = externalResources || externalResource_rec.external_uri || seperateur;
-END LOOP;
-
-SELECT 	uri, con.status, local_URI, con.id_concept, con.id_ark, prefLab, altLab, altLab_hiden, definition, example,
-          editorialNote, changeNote, secopeNote, note, historyNote, con.notation, narrower, broader, related, exactMatch, closeMatch,
-          broadMatch, relatedMatch, narrowMatch, gpsData, membre, con.created, con.modified,
-          img, creator, contributor, replaces, replacedBy, facets, externalResources INTO rec;
-
-RETURN NEXT rec;
-END LOOP;
-END;
+end;
 $BODY$;
 
-ALTER FUNCTION public.opentheso_get_concepts(character varying, character varying)
-    OWNER TO opentheso;
+
+UPDATE concept_group
+SET idgroup = LOWER(idgroup);
+
+UPDATE concept_group_historique
+SET idgroup = LOWER(idgroup);
+
+UPDATE concept_group_concept
+SET idgroup = LOWER(idgroup);
+
+UPDATE concept_group_label
+SET idgroup = LOWER(idgroup);
+
+UPDATE concept_group_label_historique
+SET idgroup = LOWER(idgroup);
