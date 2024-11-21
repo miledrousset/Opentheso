7b38fd1a92e0fc60fe33796c894231fb821b6ad1
GABSI <firas.gabsi@gmail.com>
2024-11-21 23:36:31 +0100

    Ajout du fichier rebots.txt

--- src/main/java/fr/cnrs/opentheso/repositories/ThesaurusRepository.java
+++ src/main/java/fr/cnrs/opentheso/repositories/ThesaurusRepository.java
@@ -1,22 +1,13 @@
 package fr.cnrs.opentheso.repositories;
 
 import fr.cnrs.opentheso.entites.Thesaurus;
-import fr.cnrs.opentheso.models.users.NodeUserGroupThesaurus;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.List;
+
+import java.util.Optional;
 
 
 public interface ThesaurusRepository extends JpaRepository<Thesaurus, String> {
 
-    @Query("SELECT new fr.cnrs.opentheso.models.users.NodeUserGroupThesaurus(" +
-            "t.idThesaurus, tl.title, -1, '', t.isPrivate) " +
-            "FROM ThesaurusLabel tl " +
-            "JOIN Thesaurus t ON tl.idThesaurus = t.idThesaurus " +
-            "WHERE tl.lang = :idLang " +
-            "AND tl.idThesaurus NOT IN (SELECT ugt.idThesaurus FROM UserGroupThesaurus ugt) " +
-            "ORDER BY LOWER(tl.title)")
-    List<NodeUserGroupThesaurus> getAllThesaurusWithoutGroup(@Param("idLang") String idLang);
+    Optional<Thesaurus> getThesaurusByIdArk(String idArk);
+
 }
