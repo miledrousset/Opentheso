74f7e75817b91bfb2d8e7b448178cc3cac0b430e
GABSI <firas.gabsi@gmail.com>
2025-01-30 15:42:56 +0100

    Ajout d'une API pour la recherche des utilisateurs

---
+++ src/main/java/fr/cnrs/opentheso/models/users/NodeUserResource.java
@@ -0,0 +1,12 @@
+package fr.cnrs.opentheso.models.users;
+
+import lombok.Data;
+import java.io.Serializable;
+
+
+@Data
+public class NodeUserResource implements Serializable {
+
+    private String mail;
+
+}
