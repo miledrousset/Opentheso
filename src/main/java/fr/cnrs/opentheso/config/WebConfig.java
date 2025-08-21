f376fd2eda7719a8b8e703d23033b70e64bc25fb
GABSI <firas.gabsi@gmail.com>
2025-08-21 11:50:12 +0200

    Ajout CrossOrigine dans toutes les APIs

--- src/main/java/fr/cnrs/opentheso/config/WebConfig.java
+++ src/main/java/fr/cnrs/opentheso/config/WebConfig.java
@@ -1,7 +1,9 @@
 package fr.cnrs.opentheso.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +18,19 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(loggingInterceptor)
                 .addPathPatterns("/**") // Inclut toutes les requêtes
                 .excludePathPatterns("/resources/**", "/static/**", "/d3js/**", "/v3/**", "/swagger-ui/**"); // Exclut les ressources statiques
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // toutes les routes
+                        .allowedOrigins("*") // autoriser toutes les origines
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false); // si tu n’utilises pas de cookies/sessions
+            }
+        };
     }
 }
